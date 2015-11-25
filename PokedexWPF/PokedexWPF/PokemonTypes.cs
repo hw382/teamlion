26b89a4dab6025a04de9fade4145a2655f99f3e9
hw382 <hayley.whitehorn@fdmgroup.com>
2015-11-25 16:29:59 +0000

    erroneousPokeDexIGNORE

---
+++ PokedexWPF/PokedexWPF/PokemonTypes.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokedexWPF
+{
+    class PokemonTypes : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void RaisePropertyChanged(string propertyName)
+        {
+            var localEvent = PropertyChanged;
+            if (localEvent != null)
+            {
+                localEvent.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        private string _type;
+        public string type
+        {
+            get { return _type; }
+            set
+            {
+                if (string.Equals(_type, value))
+                {
+                    return;
+                }
+                _type = value;
+                RaisePropertyChanged("type");
+            }
+        }        
+
+
+        //end of class
+    }
+}
--- PokedexWPF/PokedexWPF/ViewModel.cs
+++ PokedexWPF/PokedexWPF/ViewModel.cs
@@ -21,10 +21,6 @@ namespace PokedexWPF
         private ICommand _saveNewPokeCommand;
         public ICommand saveNewPokeCommand
         {
-            if (true)
-	{
-		 
-	}
             get
             {
                 if (_saveNewPokeCommand == null)
@@ -37,6 +33,34 @@ namespace PokedexWPF
             set { _saveNewPokeCommand = value; }
         }
 
+        private readonly IList<PokemonTypes> types = new ObservableCollection<PokemonTypes>();
+        public IList<PokemonTypes> Types
+        {
+            get
+            {
+                return types;
+            }
+        }
+
+        private PokemonTypes _selectedType;
+
+        public PokemonTypes selectedType
+        {
+            get { return _selectedType; }
+            set 
+            { 
+                if (Object.Equals(selectedType, value))
+                {
+                    return;
+                }
+                _selectedType = value;
+                OnPropertyChanged("selectedType");
+            }
+        }
+
+
+
+
         public ViewModel()
         {
             listOfPokemon = new ObservableCollection<Pokemon>()
@@ -45,9 +69,38 @@ namespace PokedexWPF
                 new Pokemon("Squirtle", "Water"),
                 new Pokemon("Bulbasaur", "Grass")              
             };
+
+            List<string> listOfTypes = new List<string>()
+            {
+                ("Normal", "Fire", "Water", "Electric", "Grass", "Ice", "Flying", "Fighting", "Poison", "Ground", "Psychic", "Bug", "Rock", "Ghost", "Steel", "Dark", "Dragon", "Fairy", "Legendary");
+            };
         }
 
 
+
+        //public ViewModel()
+        //{
+        //    types.Add(new PokemonTypes { type = "Normal" });
+        //    types.Add(new PokemonTypes { type = "Fire" });
+        //    types.Add(new PokemonTypes { type = "Water" });
+        //    types.Add(new PokemonTypes { type = "Electric" });
+        //    types.Add(new PokemonTypes { type = "Grass" });
+        //    types.Add(new PokemonTypes { type = "Ice" });
+        //    types.Add(new PokemonTypes { type = "Fighting" });
+        //    types.Add(new PokemonTypes { type = "Poison" });
+        //    types.Add(new PokemonTypes { type = "Ground" });
+        //    types.Add(new PokemonTypes { type = "Flying" });
+        //    types.Add(new PokemonTypes { type = "Psychic" });
+        //    types.Add(new PokemonTypes { type = "Bug" });
+        //    types.Add(new PokemonTypes { type = "Rock" });
+        //    types.Add(new PokemonTypes { type = "Ghost" });
+        //    types.Add(new PokemonTypes { type = "Dragon" });
+        //    types.Add(new PokemonTypes { type = "Dark" });
+        //    types.Add(new PokemonTypes { type = "Steel" });
+        //    types.Add(new PokemonTypes { type = "Fairy" });
+        //    types.Add(new PokemonTypes { type = "Legendary" });
+            
+        //}
 
         public event PropertyChangedEventHandler PropertyChanged;
         private void OnPropertyChanged(string propertyName)
@@ -62,12 +115,7 @@ namespace PokedexWPF
 
         public bool CanSaveNewPokemon()
         {
-            //if (newPokeN != null && newPokeT != null)
-            if (string.IsNullOrEmpty())
-            {
                 return true;
-            }
-
         }
 
         public void SaveNewPokemon()
